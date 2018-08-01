12d95c064f69dd99f2c8d24141c477d9fdade6e2
J Fern <jfern20@gmail.com>
2018-08-01 00:42:21 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -4,7 +4,6 @@ def square_array(array)
  squared = []
  # return value of an each loop is the org array each was called on
  array.each do |number| 
-  
    #binding.pry
    squared.push (number * number)
  end 
